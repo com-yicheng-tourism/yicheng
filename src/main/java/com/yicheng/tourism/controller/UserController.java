8382218559301b3cb0f2b04a3ed6c5b039dfc5ec
wangqingsong <wangqs9@asiainfo.com>
2020-05-02 16:13:14 +0800

    批量输入数据

--- src/main/java/com/yicheng/tourism/controller/UserController.java
+++ src/main/java/com/yicheng/tourism/controller/UserController.java
@@ -56,7 +56,6 @@ public class UserController {
 
     @ApiOperation(value = "用户按条件分页查询")
     @RequestMapping(value = "/qry",method = RequestMethod.GET)
-    @ResponseBody
     public BaseResponse<PageInfo<User>> qryByPage(UserQryConditionReq req){
         return userService.qryByCondition(req);
     }
