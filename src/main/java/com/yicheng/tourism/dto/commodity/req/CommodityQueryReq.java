dc6572b05d20fcf098d49793e8d6c825c01db4b3
SmallStartWang <1291280738@qq.com>
2020-05-12 16:10:53 +0800

    商品管理

---
+++ src/main/java/com/yicheng/tourism/dto/commodity/req/CommodityQueryReq.java
@@ -0,0 +1,24 @@
+package com.yicheng.tourism.dto.commodity.req;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel(value = "查询商品列表列表请求参数")
+@Data
+public class CommodityQueryReq {
+
+    @ApiModelProperty(value = "查询第几页",dataType = "Integer")
+    private Integer page;
+    @ApiModelProperty(value = "每页大小",dataType = "Integer")
+    private Integer rows;
+
+    @ApiModelProperty(value = "商品id",dataType = "String")
+    private String id;
+
+    @ApiModelProperty(value = "商品名称",dataType = "String")
+    private String commodityName;
+
+    @ApiModelProperty(value = "商品状态",dataType = "String")
+    private String commodityState;
+}
